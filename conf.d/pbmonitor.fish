1e9f60a3e1236e27ec01c4eaccb17af5af16328b
Dang <huynhminhdang@gmail.com>
2024-03-14 08:09:47 +0800

    format code with fish_indent

--- conf.d/pbmonitor.fish
+++ conf.d/pbmonitor.fish
@@ -1,22 +1,22 @@
 set -g PBMONITOR_LOCKFILE $TMPDIR/pbmonitor.lock
 
 function pbmonitor -a action -d 'start pbmonitor'
-  switch $action
-  case restart
-    pkill -x pbmonitor >/dev/null 2>&1
-  end
-
-  # use nohup to leave this child process running in the background after this process terminated
-  nohup fish -P -c '
-    if shlock -f '$PBMONITOR_LOCKFILE' -p $fish_pid
-      command pbmonitor | while read -l -z clip
-        emit clipboard_change "$clip"
-      end
+    switch $action
+        case restart
+            pkill -x pbmonitor >/dev/null 2>&1
     end
-  ' >/dev/null 2>&1 &
 
-  # hide warning when terminate this process
-  disown
+    # use nohup to leave this child process running in the background after this process terminated
+    nohup fish -P -c '
+        if shlock -f '$PBMONITOR_LOCKFILE' -p $fish_pid
+            command pbmonitor | while read -l -z clip
+                emit clipboard_change "$clip"
+            end
+        end
+    ' >/dev/null 2>&1 &
+
+    # hide warning when terminate this process
+    disown
 end
 
 status is-interactive || exit
